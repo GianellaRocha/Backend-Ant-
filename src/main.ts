e5690745dec2f1eb679d7d7dff3963dc979d8854
Giane <gianellarochavendivengo123@gmail.com>
2026-09-14 11:07:42 -0300

    Cambios para deploy

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
   app.enableCors({
-    origin: true,
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      process.env.ADMIN_FRONTEND_URL,
+      process.env.CATALOGO_FRONTEND_URL,
+    ].filter(Boolean) as string[],
     credentials: true,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +24,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
